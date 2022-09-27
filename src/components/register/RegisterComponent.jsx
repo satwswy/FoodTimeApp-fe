72714a5bc4f7d391bb2675d15364369c757f6d57
marios <mgeorgak859@gmail.com>
2022-09-27 23:06:42 +0300

    Updates Register Component styles

--- src/components/register/RegisterComponent.jsx
+++ src/components/register/RegisterComponent.jsx
@@ -5,35 +5,35 @@ import { Form, Button } from 'react-bootstrap'
 const RegisterComponent = () => {
     return (
         <>
-            <div className='register-div'>
-                
-                    <Form className='register-form-div'>
-                        <h1 className='h1-register'>Register</h1>
-                        <Form.Group className="mb-3" >
-                            <Form.Label className='label'>Username</Form.Label>
-                            <Form.Control className='register-text' type="text" placeholder="Username" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" >
-                            <Form.Label className='label'>Email</Form.Label>
-                            <Form.Control className='register-text' type="text" placeholder="Email"  />
-                        </Form.Group>
-                        <Form.Group className="mb-3" >
-                            <Form.Label className='label'>Date of birth</Form.Label>
-                            <Form.Control className='register-text' type="date" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" >
-                            <Form.Label className='label'>Password</Form.Label>
-                            <Form.Control className='register-text' type="text" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" >
-                            <Form.Label className='label'>Confirm Password</Form.Label>
-                            <Form.Control className='register-text' type="text" placeholder="Confirm Password" />
-                        </Form.Group>
-                        <Button className='register-button' variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-               
+            <div className='register'>
+                <h1 className='register-h1'>FOOD TIME</h1>
+                <h5 className='register-h5'>Please Register to continue</h5>
+                <Form className='register-form-div'>
+                    <h1 className='h1-register'>Register</h1>
+                    <Form.Group className="mb-3" >
+                        <Form.Label className='label'>Username</Form.Label>
+                        <Form.Control className='register-text' type="text" placeholder="Username" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label className='label'>Email</Form.Label>
+                        <Form.Control className='register-text' type="text" placeholder="Email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label className='label'>Date of birth</Form.Label>
+                        <Form.Control className='register-text' type="date" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label className='label'>Password</Form.Label>
+                        <Form.Control className='register-text' type="text" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label className='label'>Confirm Password</Form.Label>
+                        <Form.Control className='register-text' type="text" placeholder="Confirm Password" />
+                    </Form.Group>
+                    <Button className='register-button' variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
         </>
 
