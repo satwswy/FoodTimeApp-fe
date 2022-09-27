e516ec283d6d793da18a09085b6235f97b05f5d5
marios <mgeorgak859@gmail.com>
2022-09-27 17:39:47 +0300

    Adds Register login Components

---
+++ src/components/register/RegisterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RegisterComponent.css'
+
+const RegisterComponent = () => {
+  return (
+    <div className='register-div'>
+        <div className='register-form-div'></div>
+    </div>
+  )
+}
+
+export default RegisterComponent
