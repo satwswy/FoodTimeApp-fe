3d3c7c61698f8d4a0848bb1b58c55f1944496551
marios <mgeorgak859@gmail.com>
2022-10-15 20:13:14 +0300

    Update LoginComponent.jsx

--- src/components/login/LoginComponent.jsx
+++ src/components/login/LoginComponent.jsx
@@ -1,45 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './LoginComponent.css'
 import { Form, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginComponent = () => {
 
-  
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const validateForm = (event) => {
-        event.preventDefault();
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        password: undefined,
+    });
 
-       
-        
+    const {loading, error, dispatch } = useContext(AuthContext);
 
+    const navigate = useNavigate()
 
-    }
-    
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
 
+    const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post("/auth/login", credentials);
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+            navigate("/")
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+        }
+    };
     return (
         <>
-            <div className='login'>            
-                <Form className='login-form-div' onSubmit={validateForm} >
+            <div className='login'>
+                <Form className='login-form-div'>
                     <h1 className='h1-login'>Login</h1>
-                    <Form.Group className="mb-3" >
-                        <Form.Label className='label'>Email</Form.Label>
-                        <Form.Control onChange={e => setEmail(e.target.value)} className='login-text' type="text" placeholder="Email" />                      
+                    <Form.Group  className="mb-3" >
+                        <Form.Label className='label' >Username</Form.Label>
+                        <Form.Control id="username" onChange={handleChange} className='login-text' type="text" placeholder="Username" />
                     </Form.Group>
                     <Form.Group className="mb-3" >
                         <Form.Label className='label'>Password</Form.Label>
-                        <Form.Control onChange={e => setPassword(e.target.value)} className='login-text' type="text" placeholder="Password" />
+                        <Form.Control id="password" onChange={handleChange} className='login-text' type="text" placeholder="Password" />
                     </Form.Group>
-                    <Button className='login-button' variant="primary" type="submit" >
+                    <Button disabled={loading} onClick={handleClick} className='login-button' variant="primary" type="submit" >
                         <span className='span-log'>Login</span>
-                        
                     </Button>
+                    {error && <span>{error.message}</span>}
                     <h5 className='login-h5-register'>
-                        Don't have an account? 
+                        Don't have an account?
                         <Link to="/register">
-                            <span className='sign-up'>Sign up</span> 
+                            <span className='sign-up'>Sign up</span>
                         </Link>
                     </h5>
                 </Form>
