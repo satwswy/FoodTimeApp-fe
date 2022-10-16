e053107c901a07415a3f5ed69be75d9787f844d4
marios <mgeorgak859@gmail.com>
2022-10-16 20:41:05 +0300

    Update SearchContext.js

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -1,40 +1,40 @@
 import { createContext, useReducer } from "react";
 
 const initial_state = {
-    city:undefined,
-    dates:[],
-    options:{
-        adult:undefined,
-        table:undefined,
-    },
+  city: undefined,
+  dates: [],
+  options: {
+    adult: undefined,
+    table: undefined,
+  },
 };
 
-export const SearchContext = createContext(initial_state)
+export const SearchContext = createContext(initial_state);
 
-const SearchReducer = (state,action)=>{
-    switch(action.type){
-        case "NEW_SEARCH":
-            return action.payload
-        case "RESET_SEARCH":
-            return initial_state
-            default:
-            return state
-    }
-}
+const SearchReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_SEARCH":
+      return action.payload;
+    case "RESET_SEARCH":
+      return initial_state;
+    default:
+      return state;
+  }
+};
 
-export const SearchContextProvider = ({children}) => {
-    const [state,dispatch] = useReducer(SearchReducer , initial_state)
+export const SearchContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(SearchReducer, initial_state);
 
-    return(
-        <SearchContext.Provider value={{
-            city: state.city,
-            dates: state.dates,
-            options: state.options,
-            dispatch,
-          }}>
-            {children}
-        </SearchContext.Provider>
-
-    )
-
+  return (
+    <SearchContext.Provider
+      value={{
+        city: state.city,
+        dates: state.dates,
+        options: state.options,
+        dispatch,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
 };
