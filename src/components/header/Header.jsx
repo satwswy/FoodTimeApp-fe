5f2ba60e611dbda9d00122e1b9015699799f0857
marios <mgeorgak859@gmail.com>
2022-10-27 14:13:41 +0300

    updates navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
       <nav className="navbar">
         <div className="nav-container" ref={navRef}>
           <div className="nav-logo">
-          <NavLink
+            <NavLink
               to="/"
               className="nav-links"
             >
               Food Time
-            </NavLink> 
+            </NavLink>
           </div>
           <div className="nav-item">
             <NavLink
@@ -57,7 +57,7 @@ const Header = () => {
               Contact Us
             </NavLink>
           </div>
-         {!user &&<> <div className="nav-item">
+          {!user && <> <div className="nav-item">
             <NavLink
               to="/register"
               className="nav-links"
@@ -65,14 +65,21 @@ const Header = () => {
               Register
             </NavLink>
           </div>
-          <div className="nav-item">
-            <NavLink
-              to="/login"
-              className="nav-links"
-            >
-              Login
-            </NavLink>
-          </div></>}
+            <div className="nav-item">
+              <NavLink
+                to="/login"
+                className="nav-links"
+              >
+                Login
+              </NavLink>
+            </div></>}
+          {user && <> <div className="nav-item profile-div">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+            </svg> <span className="username-span">{user.username}</span>
+            
+          </div>
+          </>}
           <button className="nav-btn nav-close-btn" onClick={showNavbar}>
             <FaTimes />
           </button>
