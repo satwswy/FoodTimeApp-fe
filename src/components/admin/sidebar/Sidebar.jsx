8c8a933778c7558bf7505547909151c5053a6489
marios <mgeorgak859@gmail.com>
2022-10-30 23:26:21 +0200

    Update Sidebar.jsx

--- src/components/admin/sidebar/Sidebar.jsx
+++ src/components/admin/sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
 import StoreIcon from "@mui/icons-material/Store";
 import InsertChartIcon from "@mui/icons-material/InsertChart";
 import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
